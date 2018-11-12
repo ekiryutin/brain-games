1e97b51ac7b526fcb1c53e113902e337127e1546
Eduard Kiryutin <kiryutin@yandex.ru>
2018-11-12 18:11:07 +0000

    update 2 stage

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-import readlineSync from 'readline-sync';
+import welcome from '..';
 
-console.log('Welcome to the Brain Games!');
-
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, '+userName+'!');
-
-
+welcome();
