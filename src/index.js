9da2546bfbad35b3fa0dfe9c374a5d885540cdb2
Eduard Kiryutin <kiryutin@yandex.ru>
2018-11-13 12:23:21 +0000

    add eslint

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const welcome = () => {
   console.log('Welcome to the Brain Games!');
 
   const userName = readlineSync.question('May I have your name? ');
-  console.log('Hello, '+userName+'!');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default welcome;
-
-
